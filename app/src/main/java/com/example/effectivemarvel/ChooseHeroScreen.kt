6272115b0c74d2cffecef82e4701bdba6e9d0c34
ShelDenis <shele1-2010@mail.ru>
2025-03-18 17:53:07 +0600

    snap behavior

--- app/src/main/java/com/example/effectivemarvel/ChooseHeroScreen.kt
+++ app/src/main/java/com/example/effectivemarvel/ChooseHeroScreen.kt
@@ -1,6 +1,10 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.example.effectivemarvel
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -33,6 +38,9 @@ fun ChooseHeroScreen(navController: NavController) {
         "Please don't make the super suits green...or animated!"),
         Hero("Iron Man", "https://iili.io/JMnuDI2.png", "I AM IRON MAN"),
         Hero("Spider Man", "https://iili.io/JMnuyB9.png", "In iron suit"))
+
+    val lazyListState = rememberLazyListState()
+    val snapBehavior = rememberSnapFlingBehavior(lazyListState = lazyListState)
 
     Box(
         modifier = with (Modifier){
@@ -69,32 +77,12 @@ fun ChooseHeroScreen(navController: NavController) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(30.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            state = lazyListState,
+            flingBehavior = snapBehavior
         ) {
             for (h in heroes) {
                 item {
-//                    Column(
-//                        modifier = Modifier
-//                            .fillMaxSize()
-//                            .padding(30.dp),
-//                        horizontalAlignment = Alignment.CenterHorizontally,
-//                        verticalArrangement = Arrangement.Top
-//                    ) {
-//                        AsyncImage(
-//                            model = h.img_ref,
-//                            contentDescription = h.name,
-//                            modifier = Modifier
-//                                .width(300.dp)
-//                                .height(550.dp)
-//                        )
-//
-//                        Text(
-//                            text = h.name,
-//                            style = MaterialTheme.typography.bodyLarge,
-//                            modifier = Modifier.padding(vertical = 0.dp),
-//                            color = White
-//                        )
-//                    }
                     val shape =  RoundedCornerShape(10.dp)
                     val height = 550.dp
                     Box(
